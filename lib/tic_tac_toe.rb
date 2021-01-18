6ef43f7d93cfda0617b4aa9233fd408f93c6a7c1
Ben Altman <altmanguitar@gmail.com>
2021-01-18 03:36:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,8 +39,8 @@ class TicTacToe
     index.between?(0, 8) && !position_taken?(index)
   end
 
-  def turn_count(board)
-    board.count {|token| token == "X" || token == "O"}
+  def turn_count
+    @board.count {|token| token == "X" || token == "O"}
   end
 
 
